7e95554246e891cb76c98ca0f62dbfcc13da491b
PiotrLizon <60019392+PiotrLizon@users.noreply.github.com>
2020-02-20 22:30:32 +0100

    Add files via upload

---
+++ 02_test/conftest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+
+class MainTest:
+
+    @pytest.fixture()
+    def setup(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        yield
+        self.driver.quit()
